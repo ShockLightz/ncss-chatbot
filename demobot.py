045636de999568345532de12238043cfcc56fca8
SIT NCSS Five <sit_ncss5@shared.sydney.edu.au>
2019-01-10 16:00:07 +1100

    Added new emote function

--- demobot.py
+++ demobot.py
@@ -1,33 +1,15 @@
 # Import flask
 from flask import Flask, request
+import random
 
 # Create your app (web server)
 app = Flask(__name__)
 
-# When people visit the home page '/' use the hello_world function
-@app.route('/', methods=['GET', 'POST'])
-def hello_world():
-    return 'Hello, World!'
-
-# When people visit the ncss page /ncss use the hello_world function
-@app.route('/ncss', methods=['GET', 'POST'])
-def ncss():
-    return '<h1>NCSS</h1>'
-
-@app.route('/greet', methods=['GET', 'POST'])
-def greet():
-    name = request.values.get('text', methods=['GET', 'POST'])
-    return f'hi {name}'
-
-@app.route('/weather', methods=['GET', 'POST'])
-def weather():
-    temp = int(request.values.get('temp'))
-    cond = 'nice'
-    if temp > 30:
-        cond = 'hot'
-    elif temp < 15:
-        cond = 'cold'
-    return f'it\'s a {cond} day'
+@app.route('/emote', methods=['GET', 'POST'])
+def emote():
+    emotes = ['(✿◠‿◠)', '≧◡≦', '(▰˘◡˘▰)', '(●´ω｀●)', '(ﾉ◕ヮ◕)ﾉ*:･ﾟ✧', '（ミ￣ー￣ミ）',
+              '(づ｡◕‿‿◕｡)づ', '✌.ʕʘ‿ʘʔ.✌', '◎[▪‿▪]◎']
+    return random.choice(emotes)
 
 if __name__ == '__main__':
     # Start the web server!
